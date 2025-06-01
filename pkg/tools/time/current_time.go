f09f1709c51fac8208de087e789c6cb5d3c6ef25
defrywiltvb <defry.heryanto@hotmail.com>
2025-06-02 02:56:10 +0700

    add get current time tool

---
+++ pkg/tools/time/current_time.go
@@ -0,0 +1,36 @@
+package time
+
+import (
+	"time"
+
+	"github.com/tmc/langchaingo/llms"
+)
+
+type CurrentTimeTool struct{}
+
+func NewCurrentTimeTool() *CurrentTimeTool {
+	return &CurrentTimeTool{}
+}
+
+func (t *CurrentTimeTool) Definition() llms.Tool {
+	return llms.Tool{
+		Type: "function",
+		Function: &llms.FunctionDefinition{
+			Name:        "GetCurrentTime",
+			Description: "Get the current time for any time calculating",
+		},
+	}
+}
+
+func (t *CurrentTimeTool) Execute(toolCall llms.ToolCall) (*llms.MessageContent, error) {
+	return &llms.MessageContent{
+		Role: llms.ChatMessageTypeTool,
+		Parts: []llms.ContentPart{
+			llms.ToolCallResponse{
+				ToolCallID: toolCall.ID,
+				Name:       toolCall.FunctionCall.Name,
+				Content:    time.Now().Format(time.RFC3339Nano),
+			},
+		},
+	}, nil
+}
