549c024cd4a49a4ab84869864d5f98a8754daa43
Defry Heryanto <defry.heryanto@hotmail.com>
2025-06-21 03:37:25 +0700

    Add documentation comments for interfaces

--- pkg/calendar/service.go
+++ pkg/calendar/service.go
@@ -4,6 +4,7 @@ import "context"
 
 //go:generate mockgen -source service.go -package mock -destination mock/mock.go
 
+// Service handles interactions with a calendar provider.
 type Service interface {
 	CreateEvent(ctx context.Context, params CreateEventParams) (string, error)
 }
